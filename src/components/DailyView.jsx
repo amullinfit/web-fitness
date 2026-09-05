ae531d8053876c942454f33df91a8f7231b5e99e
amullinfit <amullin-fit@usa.net>
2026-09-05 10:27:33 -0400

    gear remove call

--- src/components/DailyView.jsx
+++ src/components/DailyView.jsx
@@ -261,7 +261,7 @@ export default function DailyView() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
-          activity_id: workoutId, 
+          activityId: workoutId, 
           gear_id: gearId 
         })
       });
