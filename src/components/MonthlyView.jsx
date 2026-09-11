b6a042aaa8468891862aa89c4b79c1110bd8a17e
amullinfit <amullin-fit@usa.net>
2026-09-11 16:23:26 -0400

    MV minimal x-axis true

--- src/components/MonthlyView.jsx
+++ src/components/MonthlyView.jsx
@@ -386,7 +386,7 @@ export default function MonthlyView() {
                       showThresholdPace={false}
                       showYAxisLabels={false}
                       showLegend={false}
-                      minimalXAxis={false}
+                      minimalXAxis={true}
                       showHoverDetails={false}
                     />
                   )}
