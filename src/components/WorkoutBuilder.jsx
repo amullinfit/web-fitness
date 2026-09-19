b9265c0078b86620964b328cf3dc5dbf517544d4
amullinfit <amullin-fit@usa.net>
2026-09-19 12:07:34 -0400

    typoe 3

--- src/components/WorkoutBuilder.jsx
+++ src/components/WorkoutBuilder.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useEffect } from 'react';
 import WorkoutChart from './WorkoutChart';
 import '../CSS/WorkoutBuilder.css';
 import { convertWorkoutToTargetFormat } from "../utils/WorkoutConverter.js";
-import { usePaces } from '../utils//PacesContext';
+import { usePaces } from '../utils//PacesContext.jsx';
 
 import { 
   fetchFoldersApi, 
