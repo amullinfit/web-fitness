0793d5104f3571c2f3ccdea926a5d181945ccc62
amullinfit <amullin-fit@usa.net>
2026-09-19 12:05:22 -0400

    typo 3

--- src/components/WorkoutChart.jsx
+++ src/components/WorkoutChart.jsx
@@ -1,6 +1,6 @@
 import React, { useId, useState } from 'react';
 import '../CSS/WorkoutChart.css';
-import { usePaces } from '../utils/PacesContext.js';
+import { usePaces } from '../utils/PacesContext.jsx';
 import {
   formatIntensityTitleCase,
   parseBoolProp,
