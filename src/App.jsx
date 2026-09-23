43f6cb24f55212664aad79d7c7e99d89d0f9d3af
amullinfit <amullin-fit@usa.net>
2026-09-23 16:54:12 -0400

    console log timestamp hh:m:ss only

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const originalLog = console.log;
 console.log = (...args) => {
   const now = new Date();
   const pad = (n) => String(n).padStart(2, '0');
-  const timestamp = `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
+  const timestamp = `${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
 
   originalLog(`[${timestamp}]`, ...args);
 };
